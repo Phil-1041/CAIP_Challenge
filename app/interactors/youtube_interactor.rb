f93de4f1693d9efee1fbd987ab79d9043a4c2417
Phil-1041 <philzheng92@gmail.com>
2020-01-16 22:07:20 -0800

    first commit

--- app/interactors/youtube_interactor.rb
+++ app/interactors/youtube_interactor.rb
@@ -1,6 +1,6 @@
 class YoutubeInteractor
 
-  KEY = 
+  KEY = 'AIzaSyBj50YHor9YUnyzlKoiAq4XhGVRSyL8pig'
   BASE_URI = 'https://www.googleapis.com/youtube/v3/'.freeze
 
   def initialize(query = '', ids = [])
